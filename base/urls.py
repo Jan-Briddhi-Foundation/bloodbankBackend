42ed12cd4367aaf23b4bd97f641bfe2a8de8cb58
dotjarvis <johnsonmakings@gmail.com>
2023-10-23 11:14:44 +0300

    Added loginPage

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home, loginPage, registrationPage, UserDetails
+from .views import home, loginPage, logoutPage, registrationPage, UserDetails, PatientHome, DonorHome
 
 urlpatterns = [
-    path('', home, name='home'),
+    # path('', home, name='home'),
     path('login/', loginPage, name='login'),
     path('registration/', registrationPage, name='registration'),
     path('logout/', logoutPage, name='logout'),
+
+    path('', PatientHome, name='home'),
+    path('donor_page/', DonorHome, name='donor_home'),
 
     path('Personal details/', UserDetails, name='user_details'),
 
--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-from .models import Profile
+from .models import Profile, User
 from .forms import CreateUserForm, UserDetailsForm
 # Create your views here.
 
@@ -35,7 +35,35 @@ def registrationPage(request):
 def loginPage(request):
     page = 'login'
 
-    context = {'page': page}
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            messages.error(request, "User does not exist")
+            return redirect('login')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.info(request, "Login successful")
+
+            profile_type = request.user.profile.profile_type
+            print(profile_type)
+
+            if profile_type == 'donor':
+                return redirect('donor_home')
+            elif profile_type == 'patient':
+                return redirect('home')
+            else:
+                return redirect('donor_home')
+        else:
+            messages.error(request, "Email or Password is incorrect")
+
+    context = {"page": page}
     return render(request, 'base/login_registration_page.html', context)
 
 
@@ -50,7 +78,6 @@ def UserDetails(request):
 
     if request.method == 'POST':
         form = UserDetailsForm(request.POST)
-        print(form)
 
         if form.is_valid():
             user_profile = request.user.profile
@@ -72,3 +99,32 @@ def UserDetails(request):
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
+
+
+# //////////////////////////////////////////////////////////////////
+# 2. DONORS PAGES
+# //////////////////////////////////////////////////////////////////
+
+
+@login_required(login_url='login')
+def DonorHome(request):
+    page = 'home_donor_page'
+
+    # blood_requests = Blood_Request.objects.all()
+
+    # context = {'page': page, "blood_requests": blood_requests}
+    context = {}
+    return render(request, 'base/donor_homePage.html', context)
+
+
+# //////////////////////////////////////////////////////////////////
+# 3. PATIENTS PAGES
+# //////////////////////////////////////////////////////////////////
+
+
+@login_required(login_url='login')
+def PatientHome(request):
+    page = 'home_patient_page'
+
+    context = {'page': page}
+    return render(request, 'base/donor_PatientPage.html', context)
