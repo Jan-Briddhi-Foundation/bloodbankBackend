772be24861efccc4f01f317c7a046e5612b9ade2
dotjarvis <johnsonmakings@gmail.com>
2023-11-13 16:31:03 +0300

    Changed the serializers.py details

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -2,27 +2,6 @@ from rest_framework import serializers
 from ..models import User, BloodGroup, Profile, Blood_Request, Donation_Form
 
 
-# class Profile_Serializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Profile
-#         fields = '__all__'
-
-
-# class Blood_Request_Serializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Blood_Request
-#         fields = '__all__'
-
-
-# class Donation_Criteria_Serializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Donation_Form
-#         fields = '__all__'
-
-
-# //////////////////////////////////////////////////
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
